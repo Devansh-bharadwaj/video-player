f05cec528deb14f3c1ce917388c99931c275a2ad
Devansh-bharadwaj <devanshbharadwaj1@gmail.com>
2024-02-15 22:04:04 +0530

    make filter and search functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,19 +5,82 @@ import { NavLink } from "react-router-dom";
 const Home = () => {
   const { videos } = useContext(VideosContext);
   const [allVideos, setAllVideos] = useState(videos.categories[0].videos);
+  const [searchInp, setSearchInp] = useState("");
+  const searchHandler = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    setSearchInp(searchTerm);
+    const filteredVideos = videos.categories[0].videos.filter(
+      (video) =>
+        video.title.toLowerCase().includes(searchTerm) ||
+        video.subtitle.toLowerCase().includes(searchTerm)
+    );
+    setAllVideos(filteredVideos);
+  };
+
+  const categories = [
+    "All",
+    "Movie",
+    "Cartoon",
+    "Recently Uploaded",
+    "Music",
+    "Web Series",
+  ];
+
+  // const filterHandler = () => {};
+
   return (
-    <div className="w-5/6 mt-14 mx-auto my-auto flex flex-wrap justify-between">
-      {allVideos?.map((video, i) => (
-        <div className="card lg:w-1/5 mt-5 mx-3 sm:w-1/2 w-full" key={video.id}>
-          <NavLink to={`/video/${video.id}`}>
-            <img src={video.thumb} alt={video.title} className="rounded-t-lg" />
-            <div>
-              <p className="m-2">{video.title + " - " + video.subtitle}</p>
-            </div>
-          </NavLink>
+    <>
+      <div className="flex justify-between flex-wrap w-5/6 m-auto mt-6">
+        <ul className="w-1/2 list-none flex flex-nowrap overflow-x-scroll scrollbar-hidden justify-between">
+          {categories.map((category) => (
+            <li
+              onClick={() => {
+                if (category === "All") {
+                  setAllVideos(videos.categories[0].videos);
+                } else {
+                  const filteredVideos = videos.categories[0].videos.filter(
+                    (video) =>
+                      video.category.toLowerCase() === category.toLowerCase()
+                  );
+                  setAllVideos(filteredVideos);
+                }
+              }}
+              className="cursor-pointer border rounded px-4 py-2 inline-block mr-2 max-w-40 whitespace-nowrap"
+            >
+              {category}
+            </li>
+          ))}
+        </ul>
+        <div className="w-1/2 text-right">
+          <input
+            type="text"
+            className="rounded-lg w-5/6 p-2 border"
+            value={searchInp}
+            onChange={searchHandler}
+            placeholder="Search"
+          />
         </div>
-      ))}
-    </div>
+      </div>
+      <div className="w-5/6 mt-9 mx-auto my-auto flex flex-wrap justify-between">
+        {allVideos?.map((video, i) => (
+          <div
+            className="card lg:w-1/5 mt-5 mx-3 sm:w-1/2 w-full"
+            key={video.id}
+          >
+            <NavLink to={`/video/${video.id}`}>
+              <img
+                src={video.thumb}
+                alt={video.title}
+                className="rounded-t-lg"
+              />
+              <div>
+                <p className="m-2">{video.title + " - " + video.subtitle}</p>
+              </div>
+            </NavLink>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/context/VideosContext.js
+++ src/context/VideosContext.js
@@ -10,6 +10,7 @@ export const VideosProvider = ({ children }) => {
         videos: [
           {
             id: 1,
+            category: "cartoon",
             description:
               "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
             sources: [
@@ -22,6 +23,7 @@ export const VideosProvider = ({ children }) => {
           },
           {
             id: 2,
+            category: "movie",
             description: "The first Blender Open Movie from 2006",
             sources: [
               "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
@@ -33,6 +35,7 @@ export const VideosProvider = ({ children }) => {
           },
           {
             id: 3,
+            category: "web series",
             description:
               "HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.",
             sources: [
@@ -45,6 +48,7 @@ export const VideosProvider = ({ children }) => {
           },
           {
             id: 4,
+            category: "music",
             description:
               "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when Batman's escapes aren't quite big enough. For $35. Learn how to use Chromecast with Google Play Movies and more at google.com/chromecast.",
             sources: [
@@ -57,6 +61,7 @@ export const VideosProvider = ({ children }) => {
           },
           {
             id: 5,
+            category: "music",
             description:
               "Introducing Chromecast. The easiest way to enjoy online video and music on your TV. For $35.  Find out more at google.com/chromecast.",
             sources: [
@@ -69,6 +74,7 @@ export const VideosProvider = ({ children }) => {
           },
           {
             id: 6,
+            category: "movie",
             description:
               "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for the times that call for bigger joyrides. For $35. Learn how to use Chromecast with YouTube and more at google.com/chromecast.",
             sources: [
@@ -81,6 +87,7 @@ export const VideosProvider = ({ children }) => {
           },
           {
             id: 7,
+            category: "web series",
             description:
               "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when you want to make Buster's big meltdowns even bigger. For $35. Learn how to use Chromecast with Netflix and more at google.com/chromecast.",
             sources: [
@@ -93,6 +100,7 @@ export const VideosProvider = ({ children }) => {
           },
           {
             id: 8,
+            category: "cartoon",
             description:
               "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
             sources: [
@@ -105,6 +113,7 @@ export const VideosProvider = ({ children }) => {
           },
           {
             id: 9,
+            category: "recently uploaded",
             description:
               "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.",
             sources: [
@@ -117,6 +126,7 @@ export const VideosProvider = ({ children }) => {
           },
           {
             id: 10,
+            category: "recently uploaded",
             description:
               "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org",
             sources: [
@@ -127,9 +137,9 @@ export const VideosProvider = ({ children }) => {
               "https://storage.googleapis.com/gtv-videos-bucket/sample/images/TearsOfSteel.jpg",
             title: "Tears of Steel",
           },
-          ,
           {
             id: 11,
+            category: "music",
             description:
               "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when you want to make Buster's big meltdowns even bigger. For $35. Learn how to use Chromecast with Netflix and more at google.com/chromecast.",
             sources: [
@@ -142,6 +152,7 @@ export const VideosProvider = ({ children }) => {
           },
           {
             id: 12,
+            category: "recently uploaded",
             description:
               "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
             sources: [
